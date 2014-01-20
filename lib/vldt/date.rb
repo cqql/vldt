cd322f1c59115e62b149a220b1be51e4dc687f59
Marten Lienen <marten.lienen@gmail.com>
2014-01-20 18:59:41 +0100

    Add date* validations

--- lib/vldt/date.rb
+++ lib/vldt/date.rb
@@ -1,18 +1,45 @@
 require "date"
 
 module Vldt
+  # Date validations.
   module Date
     # Validate, that an object can be parsed as a date.
     def date
       Predicate.new(:date) do |o|
         begin
-          ::Date.parse(o)
+          o.is_a?(::Date) or ::Date.parse(o)
 
           true
         rescue TypeError, ArgumentError
           false
         end
       end
+    end
+
+    # Validate, that a date is after (+>=+) +date+.
+    def date_after (date)
+      Predicate.new(:date_after, { date: date }) do |o|
+        o >= date
+      end
+    end
+
+    # Validate, that a date is after (+<=+) +date+.
+    def date_before (date)
+      Predicate.new(:date_before, { date: date }) do |o|
+        o <= date
+      end
+    end
+
+    # Validate, that a date is in an allowed range.
+    def date_between (min, max)
+      Predicate.new(:date_between, { min: min, max: max}) do |o|
+        o >= min && o <= max
+      end
+    end
+
+    # Validate an object after parsing it as a {::Date}.
+    def with_date (validation)
+      With.new(-> d { ::Date.parse(d) }, validation)
     end
 
     extend(self)
--- spec/vldt/date_spec.rb
+++ spec/vldt/date_spec.rb
@@ -9,17 +9,61 @@ describe Vldt::Date do
     it "should succeed if the object can be parsed as a Date" do
       expect(v.call("20140120")).to eq nil
     end
+
+    it "should succeed if the object is a date" do
+      expect(v.call(Date.today)).to eq nil
+    end
   end
 
-  describe "#iso8601_date" do
-    let(:v) { Vldt::Date.iso8601_date }
+  describe "#date_after" do
+    let(:v) { Vldt::Date.date_after(Date.new(2014, 1, 2)) }
 
-    it "should fail if the object is not an iso8601 date" do
-      expect(v.call("2013-1w-10")).to eq({ [] => [[:iso8601_date, {}]]})
+    it "should fail if the date is before the given one" do
+      expect(v.call(Date.new(2013, 12, 12))).to eq ({ [] => [[:date_after, date: Date.new(2014, 1, 2)]] })
     end
 
-    it "should succeed if the object is an iso8601 date" do
-      expect(v.call("2014-01-19")).to eq nil
+    it "should succeed if the date is after the given one" do
+      expect(v.call(Date.new(2014, 5, 6))).to eq nil
+    end
+  end
+
+  describe "#date_before" do
+    let(:v) { Vldt::Date.date_before(Date.new(2005, 1, 2)) }
+
+    it "should fail if the date is after the given one" do
+      expect(v.call(Date.new(2013, 12, 12))).to eq ({ [] => [[:date_before, date: Date.new(2005, 1, 2)]] })
+    end
+
+    it "should succeed if the date is before the given one" do
+      expect(v.call(Date.new(2000, 1, 1))).to eq nil
+    end
+  end
+
+  describe "#date_between" do
+    let(:v) { Vldt::Date.date_between(Date.new(2010, 1, 1), Date.new(2012, 6, 6)) }
+
+    it "should fail if the date is before the minimum" do
+      expect(v.call(Date.new(2000, 1, 1))).to eq({ [] => [[:date_between, min: Date.new(2010, 1, 1), max: Date.new(2012, 6, 6)]] })
+    end
+
+    it "should fail if the date is after the maximum" do
+      expect(v.call(Date.new(2015, 1, 1))).to eq({ [] => [[:date_between, min: Date.new(2010, 1, 1), max: Date.new(2012, 6, 6)]] })
+    end
+
+    it "should succeed if the date is in the allowed range" do
+      expect(v.call(Date.new(2011, 2, 28))).to eq nil
+    end
+  end
+
+  describe "#with_date" do
+    let(:v) { Vldt::Date.with_date(Vldt::Date.date_after(Date.new(2008, 3, 2))) }
+
+    it "should parse the object as a date" do
+      expect(v.call("2014-02-01")).to eq nil
+    end
+
+    it "should forward errors" do
+      expect(v.call("19950101")).to eq({ [] => [[:date_after, date: Date.new(2008, 3, 2)]] })
     end
   end
 end
